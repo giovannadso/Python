93e472ebe97e7605e761ceae38ff2107aa9d23f7
giovannadso <127310419+giovannadso@users.noreply.github.com>
2023-05-19 11:50:03 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,33 @@
+print ("Mensagem que eu quero", "TESTE")
+
+print ("Mensagem 2")
+
+print ('Fulano é meu "amigo"')
+
+print ("Fulano \né meu \"amigo\"")
+
+print (1+1)
+
+print ("1 x 1 =",1*1)
+
+print ("10³ =", 10**3)
+
+print ("10³ =", pow (10,3))
+
+
+
+nome = ""
+print (nome)
+nome = 1
+print (nome)
+nome = 1+1
+print (nome)
+nome = True
+print (nome)
+
+D1 = 5.6
+D2 = "5"
+print (int(D1 + int (D2)))
+print (round(D1 + int (D2)))
+
+
